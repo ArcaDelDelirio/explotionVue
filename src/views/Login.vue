c54f28e1585a3eb17257415fa85ea847c323439c
ArcaDelDelirio <elarcadeldelirio@gmail.com>
2019-07-04 15:28:45 +0100

    Something

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <NavData />
+    <h1>This is the Login page</h1>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import NavData from '@/components/NavData.vue'
+
+export default {
+  name: 'about',
+  components: {
+    NavData,
+  }
+}
+</script>
