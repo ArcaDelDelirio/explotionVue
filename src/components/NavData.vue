cf85f7d163d07a97dc903d456299aa1c4a9d9ed3
ArcaDelDelirio <elarcadeldelirio@gmail.com>
2019-07-13 10:48:55 +0100

    Format navbar

--- src/components/NavData.vue
+++ src/components/NavData.vue
@@ -7,27 +7,29 @@ export default {
 
 <template>
     <nav>
-        <router-link to="/">Home</router-link> 
-        <router-link to="/about">About</router-link>
-        <router-link to='/login'>Login</router-link>
-        <router-link to='/register'>Register</router-link>
-        <img src="@/assets/movie.png" height="24px" width="24px" alt="logo">
+        <ul class="container">
+            <router-link to="/">Home</router-link>
+            <router-link to="/about">About</router-link>
+            <router-link to='/login'>Login</router-link>
+            <router-link to='/register'>Register</router-link>
+            <img src="@/assets/movie.png" height="24px" width="24px" alt="logo">
+        </ul>
     </nav>
 </template>
 
 
 <style scoped>
-    nav{
+    .container{
         display: flex;
         justify-content: space-between;
         background-color: grey;
         margin:0;
         padding:1%;
     }
-    nav a{
+    .container a{
         align-self: center;
     }
-    nav img{
+    .container img{
         align-self: center;
     }
 </style>
